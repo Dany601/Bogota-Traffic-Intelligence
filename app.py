b44aa72e63dda4a17720e3e040c3352264051121
Daniela <danielaparra1104@gmail.com>
2026-05-08 20:28:45 -0500

    Configurar el servidor para el despliegue en producción y eliminar archivo de configuración de gunicorn

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
+import os
 import pandas as pd
 import numpy as np
 from flask import Flask, render_template, request
-import webbrowser
-from threading import Timer
 import requests
 import io
 import base64
@@ -180,4 +179,5 @@ def index():
     return "Error al procesar datos."
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
